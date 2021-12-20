9aa4f66f4aa6eb699055f6a230d16001f93ae494
Peter Mayer <peter.mayer@isb.bayern.de>
2021-12-20 21:00:57 +0100

    Adding Path to env file

--- powerbutton_install.sh
+++ powerbutton_install.sh
@@ -3,7 +3,7 @@
 # Colors: \e[36m=Cyan M ; \e[92m=Light green ; \e[91m=Light red ; \e[93m=Light yellow ; \e[31m=green ; \e[0m=Default ; \e[33m=Yellow ; \e[31m=Red
 
 #branch="development"
-version="1.0.0 - 20211220"
+version="1.0.1 - 20211220"
 repo="https://github.com/PM84/raspberry_pi_power_on_off.git"
 branch="main"
 
@@ -195,7 +195,7 @@ while [ ${gpioready} = 0 ]; do
 	do
 		case $opt in
 			"GPIO settings are OK")
-                file=".env"
+                file=${installPath}/".env"
                 sudo echo "SD_GPIO=${sdgpio}" > $file
 				gpioready=1
 				break
