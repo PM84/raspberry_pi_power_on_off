466d44eb98bf53d503e56cab0d34540f343e7760
Peter Mayer <peter.mayer@isb.bayern.de>
2021-12-20 22:13:10 +0100

    1.0.7

--- shutdownlistener.py
+++ shutdownlistener.py
@@ -3,11 +3,20 @@
 
 import RPi.GPIO as GPIO
 import subprocess
+import time
+
 
 SD_GPIO = 3 # You can not change this pin.
+BUTTON_PRESSED_FOR_SEC = 2
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SD_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(SD_GPIO, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+  GPIO.wait_for_edge(SD_GPIO, GPIO.FALLING)
+  time.sleep(BUTTON_PRESSED_FOR_SEC)
+  if GPIO.input(SD_GPIO) == False:
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+    break
+
+GPIO.cleanup()
