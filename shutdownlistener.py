a6b67efdb6c48cb2090721af529756c2b3973559
Peter Mayer <peter.mayer@isb.bayern.de>
2021-12-20 21:11:41 +0100

    Fix issue with module path

--- shutdownlistener.py
+++ shutdownlistener.py
@@ -3,8 +3,8 @@
 
 import RPi.GPIO as GPIO
 import subprocess
-import os
-from config import *
+
+from home.pi.raspberry_pi_power_on_off.config import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SD_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
